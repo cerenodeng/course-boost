a318b576c9c1a081fe1078d5de610b0230018749
Cereno Deng <cereno.deng@outlook.com>
2023-05-16 03:16:30 +0800

    add header

--- app/layout.js
+++ app/layout.js
@@ -2,13 +2,31 @@ import './globals.css'
 
 export const metadata = {
   title: 'Course Boost UI',
-  description: '',
+  description: 'Best online course management and selling platform',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
-    </html>
+      <body>
+        <div style={{
+          backgroundImage: "url('https://picsum.photos/1920/1080')"
+        }} class="h-screen bg-center bg-cover">
+          <div class="flex p-4">
+            <div class="flex w-1/2 justify-start gap-x-5 px-5 py-2.5">
+              <a href="#" class="text-lg font-normal hover:underline hover:underline-offset-8">Demo</a>
+              <a href="#" class="text-lg font-normal hover:underline hover:underline-offset-8">Pricing</a>
+              <a href="#" class="text-lg font-normal hover:underline hover:underline-offset-8">About</a>
+            </div>
+            <div class="flex w-1/2 justify-end gap-x-5 px-5 py-2.5">
+              <a href="#" class="text-lg font-normal hover:underline hover:underline-offset-8">Join</a>
+              <a href="#" class="text-lg font-normal hover:underline hover:underline-offset-8">Login</a>
+            </div>
+          </div>
+        </div>
+
+        {children}
+      </body>
+    </html >
   )
 }
--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-between p-24">
-      Course Boost UI
+      Course Boost is a online course management and selling platform
     </div>
   )
 }
