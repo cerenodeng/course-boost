1bf0825b586a2dc12c9bd1b39e13995bbf7e601c
Cereno Deng <cereno.deng@outlook.com>
2023-06-30 16:27:07 +0800

    change class to className

--- app/layout.js
+++ app/layout.js
@@ -11,16 +11,16 @@ export default function RootLayout({ children }) {
       <body>
         <div style={{
           backgroundImage: "url('https://picsum.photos/1920/1080')"
-        }} class="h-screen bg-center bg-cover">
-          <div class="flex p-4">
-            <div class="flex w-1/2 justify-start gap-x-5 px-5 py-2.5">
-              <a href="#" class="text-lg font-normal hover:underline hover:underline-offset-8">Demo</a>
-              <a href="#" class="text-lg font-normal hover:underline hover:underline-offset-8">Pricing</a>
-              <a href="#" class="text-lg font-normal hover:underline hover:underline-offset-8">About</a>
+        }} className="h-screen bg-center bg-cover">
+          <div className="flex p-4">
+            <div className="flex w-1/2 justify-start gap-x-5 px-5 py-2.5">
+              <a href="#" className="text-lg font-normal hover:underline hover:underline-offset-8">Demo</a>
+              <a href="#" className="text-lg font-normal hover:underline hover:underline-offset-8">Pricing</a>
+              <a href="#" className="text-lg font-normal hover:underline hover:underline-offset-8">About</a>
             </div>
-            <div class="flex w-1/2 justify-end gap-x-5 px-5 py-2.5">
-              <a href="#" class="text-lg font-normal hover:underline hover:underline-offset-8">Join</a>
-              <a href="#" class="text-lg font-normal hover:underline hover:underline-offset-8">Login</a>
+            <div className="flex w-1/2 justify-end gap-x-5 px-5 py-2.5">
+              <a href="#" className="text-lg font-normal hover:underline hover:underline-offset-8">Join</a>
+              <a href="#" className="text-lg font-normal hover:underline hover:underline-offset-8">Login</a>
             </div>
           </div>
         </div>
